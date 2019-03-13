fec9059d53921217bd4aacf04fcbe1bf1c599b04
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-13 13:43:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,13 +5,17 @@ class String
   def sentence?
     "Hi, My name is Sophie".end_with.
   end
-def sentence?
+
+  def sentence?
     "Hi, My name is Sophie".end_with
   end
 
   def exclamation?
     "What's your name?".end_with("?")
   end
+
+  def exclamation?
+    
 
   def count_sentences
 
