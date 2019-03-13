2db99009f3620b7dcfa96009dd25825ca812d58c
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-13 13:54:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "Hi, My name is Sophie".end_with.
   end
 
-  def question?
-
+  def sentence?
+    "Hi, My name is Sophie".end_with
   end
 
   def exclamation?
+    "What's your name?".end_with("?")
+  end
 
+  def exclamation?
+    "Happy Halloween".end_with
   end
 
   def count_sentences
-
+    "one, two, three?".count_sentences
   end
-end
